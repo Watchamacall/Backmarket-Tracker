5716a8a9121a0c7288993661201578dfd935a15b
Ryan Cole <rc248809@falmouth.ac.uk>
2023-01-09 00:31:33 +0000

    Created bat file for Windows Task Scheduler

---
+++ backmarket_batch.bat
@@ -0,0 +1,7 @@
+::You cannot have spaces with bat files, make sure backmarket_file_location does not have a space when making
+backmarket_file_location="F:\FALMOUTH\Random Repos\Backmarket-Tracker"
+backmarket_file_name="backmarket_tracker.py"
+
+cd backmarket_file_location
+
+python backmarket_file_name
